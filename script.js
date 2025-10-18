55e8a8c0d1ed10841335a00f57e12aa0e79e211c
theriakhurana <theriakhurana@gmail.com>
2025-10-19 01:06:46 +0530

    added board layout

--- script.js
+++ script.js
@@ -14,9 +14,40 @@ let pinkGhost;
 let redGhost;
 let pacmanUp;
 let pacmanDown;
-let packmanLeft;
-let packmanRight;
-let wall;
+let pacmanLeft;
+let pacmanRight;
+let wallImage;
+
+//X = wall, O = skip, P = pac man, ' ' = food
+//Ghosts: b = blue, o = orange, p = pink, r = red
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX" 
+];
+
+const walls = new Set();
+const foods = new Set();
+const ghosts = new Set();
+let pacman;
 
 window.onload = function() {
   board = document.getElementById("board");
@@ -25,6 +56,10 @@ window.onload = function() {
   context = board.getContext("2d"); // used for drawing on the board
 
   loadImages();
+  loadMap();
+  console.log(walls.size);
+  console.log(foods.size);
+  console.log(ghosts.size);
 }
 
 function loadImages() {
@@ -49,4 +84,61 @@ function loadImages() {
   pacmanRight = new Image();
   pacmanRight.src = "./images/pacmanRight.png";
 
+}
+
+function loadMap() {
+  walls.clear();
+  foods.clear();
+  ghosts.clear();
+
+  for (let r = 0; r < rowCount; r++) {
+    for (let c = 0; c < colCount; c++) {
+      const row = tileMap[r];
+      const tileMapChar = row[c];
+
+      const x = c * tileSize;
+      const y = r * tileSize
+
+      if (tileMapChar == 'X') { //block wall 
+        const wall = new Block(wallImage, x, y, tileSize, tileSize);
+        walls.add(wall);
+      } 
+      else if (tileMapChar == 'b') { // blue ghost
+        const ghost = new Block(blueGhost, x, y, tileSize, tileSize);
+        ghosts.add(ghost);
+      }
+      else if (tileMapChar == 'p') { // pink ghost
+        const ghost = new Block(pinkGhost, x, y, tileSize, tileSize);
+        ghosts.add(ghost);
+      }
+      else if (tileMapChar == 'o') { // orange ghost
+        const ghost = new Block(orangeGhost, x, y, tileSize, tileSize);
+        ghosts.add(ghost);
+      }
+      else if (tileMapChar == 'r') { // red ghost
+        const ghost = new Block(redGhost, x, y, tileSize, tileSize);
+        ghosts.add(ghost);
+      }
+      else if (tileMapChar == 'P') { // pacman
+        pacman = new Block(pacmanRight, x, y, tileSize, tileSize);
+      }
+      else if (tileMapChar == ' ') { // empty is food
+        const food = new Block(null, x+14, y+14, 4, 4);
+        foods.add(food);
+      }
+    }
+  }
+}
+
+class Block{
+  constructor(image, x, y, width, height) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.startX = x;
+    this.startY = y;
+  }
 }
